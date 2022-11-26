51384ad7ed792fe751a547a63ed7bc5d69538086
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2022-11-26 06:34:10 -0500

    Added started files for Hat Check exercise

---
+++ hatcheck/src/main/java/com/example/hatchcheck/HatCheck.java
@@ -0,0 +1,4 @@
+package com.example.hatchcheck;
+
+public class HatCheck {
+}
---
+++ hatcheck/src/test/java/com/example/hatchcheck/HatCheckTest.java
@@ -0,0 +1,7 @@
+package com.example.hatchcheck;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HatCheckTest {
+  
+}
